113c08f72b5e2ddf80f01da2b22dbea114298810
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-04-05 16:01:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,16 @@ const modal = document.getElementById("modal");
 const openBtn = document.getElementById("openModal");
 const closeBtn = document.querySelector(".close-modal");
 
-// Open modal
-openBtn.onclick = () => {
+openBtn.onclick = function() {
   modal.style.display = "block";
-};
+}
 
-// Close when clicking the close button
-closeBtn.onclick = () => {
+closeBtn.onclick = function() {
   modal.style.display = "none";
-};
+}
 
-// Close when clicking outside modal-content
-window.onclick = (event) => {
-  if (event.target === modal) {
+window.onclick = function(event) {
+  if (event.target == modal) {
     modal.style.display = "none";
   }
-};
+}
